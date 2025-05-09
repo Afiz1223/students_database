29f0271e4663e20a17169e360e44abc28ab607f2
Akpati Ifeoma <akpati29ifeoma@gmail.com>
2025-05-09 12:16:08 +0100

    first initialize

---
+++ app/Models/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Image extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+
+    Public function user():BelongsTo {
+        return $this-> belongsTo(user::class);
+    }
+
+}
+
+
+
---
+++ database/migrations/2025_05_08_122414_create_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
+
+// php artisan migrate:rollback --path=database/migrations/2023_01_01_000000_create_users_table.php
+// php artisan migrate --path=database/migrations/2023_10_15_000000_create_table_name_table.php
